8f8a0b6687ae6419f16269c6a8f7774a50278f61
Daniel Lek <1902640@sit.singaporetech.edu.sg>
2020-11-20 02:28:54 +0800

    forgot to push posting into github

--- posting.php
+++ posting.php
@@ -1,58 +1,37 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+        <meta charset="UTF-8"><title>posting page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--<link rel="stylesheet" href ="js/posting.js"></link>-->
+        <!--<script src="js/posting.js"></script>-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">        
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <!--<script defer src="js/posting.js" type="text/javascript"></script>-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    </head>
 <?php
-
-if (!isset($_SESSION['name'])) {
-  header("Location: login.php");
-  exit();
-}else {
 
 include "header.inc.php";
 
-define("DBHOST", "localhost");
-define("DBNAME", "2103");
-define("DBUSER", "root");
-define("DBPASS", "");
+if (!isset($_SESSION['nric'])) {
+  header("Location: login.php");
+  exit();
+}else {
+    
+if(isset($_POST['postingSubmit']))
+{
+   submitposting();
+} 
 
 
 $fname = $lname = $email = $pwd = $cpwd = $errorMsg = "";
-    $photo = "moe.png";
-    echo  '<img src="images/MOE.png" alt="moe" class = "center"> ';
-    echo '<section class="container"><hr>';
-    echo "<h1>S1 OPTION FORM </h1>";
-    echo "<h2>Student's Particulars </h2>";
-    echo "<p>Name of students: </p>";
-    echo "<p>Primary School: </p>";
-    echo "<p>Year of PSLE: </p>";
-    echo "<p>PSLE Index No: </p>";
-    echo "<p>Citizenship: </p>";
-    echo "<p>PSLE Aggregate: </p>";
-    echo "<p>Course Eligible </p>";
-    saveMemberToDB();
+    displayStudentInfo();
 
-
-
-function saveMemberToDB(){
-    global $fname, $lname, $email, $pwd, $errorMsg, $success;
-    // Create connection
-    $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-    //Check connection
-    if ($conn->connect_error)
-    {$errorMsg = "Connection failed: " . $conn->connect_error;
-    $success = false;
-    }
-    else
-    {
-    $sql = "INSERT INTO userlogin (firstname, lastname, email, password)";$sql .= " VALUES('$fname', '$lname', '$email', '$pwd')";
-    // Execute the queryif (!$conn->query($sql))   
-    if (!$conn->query($sql))
-    {
-        $errorMsg = "Database error: " . $conn->error;
-        $success = false;
-        
-    }   
-  }
-  $conn->close();
-}
 ?>
      
      
@@ -61,24 +40,7 @@ function saveMemberToDB(){
      
     <body>    
     <div class="container">
-    <h2>Parent's/ Guardian's Local Contact Details</h2>
-    <form name="myForm" action="process_posting.php"  novalidate onsubmit="return validateForm()" method="post">
-      
-      
-    <div class="form-group">
-    <label for="name">Name:</label>
-    <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
-    </div>
-      
-    <div class="form-group">
-    <label for="mobileNo">Mobile No:</label>
-    <input type="text" class="form-control" id="mobileNo" placeholder="Enter Mobile Number" name="mobileNo">
-    </div>
-      
-    <div class="form-group">
-    <label for="contactNo">Contact No:</label>
-    <input type="text" class="form-control" id="contactNo" placeholder="Enter Contact Number" name="contactNo">
-    </div>
+    <form name="myForm" action="posting.php"  novalidate onsubmit="return validateForm()" method="post">
       
       
     <h2>Choices of Secondary Schools</h2>  
@@ -117,7 +79,7 @@ function saveMemberToDB(){
     
     
       
-    <button type="submit" value = "Submit now" class="btn btn-default">Submit</button>
+    <button type="submit" name="postingSubmit" value = "Submit now" class="btn btn-default">Submit</button>
   </form>
   
   
@@ -126,6 +88,168 @@ function saveMemberToDB(){
 <?php
         include "footer.inc.php";
 }
+
+
+
+
+function displayStudentInfo(){
+  global $errorMsg, $success;
+
+  $studentInfo = [];
+  $studentInfo = getStudentInfo($_SESSION['nric']);
+
+      $photo = "moe.png";
+      echo  '<img src="images/MOE.png" alt="moe" class = "center"> ';
+      echo '<section class="container"><hr>';
+      echo '<h1>S1 OPTION FORM </h1>';
+      echo '<h2>Students Particulars </h2>';
+      echo '<p>Name of students: '.$studentInfo[0].'</p>';
+      echo '<p>Primary School:'.$studentInfo[1].'</p>';
+      echo '<p>Year of PSLE: '.$studentInfo[2].'</p>';
+  //    echo "<p>PSLE Index No: </p>";
+      echo '<p>Citizenship: '.$studentInfo[3].'</p>';
+      echo '<p>PSLE Aggregate: '.$studentInfo[4].'</p>';
+      
+      
+ 
+}
+
+function getStudentInfo($nric){
+    
+  $manager = new MongoDB\Driver\Manager('mongodb+srv://kinseong:sceptile101@cluster.dqjim.mongodb.net/ICT2103?retryWrites=true&w=majority');
+  
+  $command = new MongoDB\Driver\Command(['ping' => 1]);
+  try {
+      $cursor = $manager->executeCommand('admin', $command);
+  } catch(MongoDB\Driver\Exception $e) {
+      echo $e->getMessage(), "\n";
+      exit;
+  }
+  $filter  = ['NRIC' => $nric]; //for student Nric change if necessary
+  $options = ['limit' => 1];
+  $sName = $name = ""; //for storing names
+  $studentInfo = [];
+  
+//    ----------------------------------------------------------------------Query for student info------------------------------------------------
+  $query1 = new \MongoDB\Driver\Query($filter, $options);
+  $document = $manager->executeQuery('ICT2103.school', $query1);
+
+  foreach ($document as $doc) {
+    $row = (array)$doc;
+    array_push($studentInfo, $row['Name']);
+    array_push($studentInfo, $row['Previous_Primary_School']);
+    array_push($studentInfo, $row['Year_of_PSLE']);
+     array_push($studentInfo, $row["Nationality"]);
+    array_push($studentInfo, $row['PSLE_Aggregate_Score']);
+  }
+  
+  
+  //if else to see if nric is student or guardian
+ 
+  
+  return $studentInfo;
+
+
+}
+
+  
+    function submitposting(){
+        
+        
+         if(!empty($_POST["firstchoice"]) 
+            && (!empty($_POST["secondchoice"])) 
+            && (!empty($_POST["thirdchoice"]))
+            && (!empty($_POST["fourthchoice"]))
+            && (!empty($_POST["fifthchoice"]))
+            && (!empty($_POST["sixthchoice"])))
+         
+        {
+
+        $manager = new MongoDB\Driver\Manager('mongodb+srv://kinseong:sceptile101@cluster.dqjim.mongodb.net/ICT2103?retryWrites=true&w=majority');
+
+        $command = new MongoDB\Driver\Command(['ping' => 1]);
+        try {
+            $cursor = $manager->executeCommand('admin', $command);
+        } catch(MongoDB\Driver\Exception $e) {
+            echo $e->getMessage(), "\n";
+            exit;
+        }
+        
+        $filter  = ['nric' => $_SESSION['nric'], 'posting_id' => ['$exists' => true]]; //search for nric and posting id if any matches
+        $filter2 = ['nric' => ['$exists' => true], 'posting_id' => ['$exists' => true]]; //get all the posting id for evaluating
+        $options = [];
+//        $name = ""; //for storing names
+        $studentInfo = $postingID = $allPostingID = [];
+        $studentInfo = getStudentInfo($_SESSION['nric']);
+        $countPostingID = 0;
+        $maxPostingID = 0;
+        $newPostingID = 0;
+
+      //    ----------------------------------------------------------------Query for if the student has submitted before------------------------------------------------
+        $query1 = new \MongoDB\Driver\Query($filter, $options);
+        $document = $manager->executeQuery('ICT2103.school', $query1);
+
+        foreach ($document as $doc) {
+          $row = (array)$doc;
+          
+          array_push($postingID, $row['posting_id']);
+        }
+        
+        
+      //    ----------------------------------------------------------------Query for all the posting id to insert next highest------------------------------------------------
+        $query2 = new \MongoDB\Driver\Query($filter2, $options);
+        $document2 = $manager->executeQuery('ICT2103.school', $query2);
+
+        foreach ($document2 as $doc) {
+          $row = (array)$doc;
+          
+          array_push($allPostingID, $row['posting_id']);
+          $countPostingID++;
+        }
+        
+        if($countPostingID > 0){ //only get the max if there is anything in the array
+        $maxPostingID = max($allPostingID);
+        }
+        
+        
+        
+//        ------------------------checking if posting form submitted already--------------------------------------------------
+        if (count($postingID) > 0){
+            echo "<script>alert('you have already submitted your posting form');</script>";
+        }
+        else{
+//        --------------------------------create new posting for student if no posting submitted under this user before-------------------------
+            if($countPostingID > 0){// if there have been postings in database then assign the next highest posting id for this entry
+                $bulk = new MongoDB\Driver\BulkWrite;
+                $bulk->insert([
+                'posting_id' => ($maxPostingID+1),
+                'student_Name' => $studentInfo[0],
+                'nric'=>$_SESSION['nric'],
+                'ranking'=>[$_POST["firstchoice"],$_POST["secondchoice"],$_POST["thirdchoice"],$_POST["fourthchoice"],$_POST["fifthchoice"],$_POST["sixthchoice"]],
+                ]);
+            
+                $manager->executeBulkWrite('ICT2103.school', $bulk);
+            }else{// if there have been postings in database then assign the next highest posting id for this entry
+                $bulk = new MongoDB\Driver\BulkWrite;
+                $bulk->insert([
+                'posting_id' => 0,
+                'student_Name' => $studentInfo[0],
+                'nric'=>$_SESSION['nric'],
+                'ranking'=>[$_POST["firstchoice"],$_POST["secondchoice"],$_POST["thirdchoice"],$_POST["fourthchoice"],$_POST["fifthchoice"],$_POST["sixthchoice"]],
+                ]);
+            
+                $manager->executeBulkWrite('ICT2103.school', $bulk);
+            }
+
+        
+        }
+
+        }else{
+                echo "<script>alert('please fill up all fields!');</script>";
+        }
+    }
+    
+
 ?>
 
 </html>
